86e5930a55ff4dc5b11598c4a33a48bafc686880
zhanghailang <1187000351@qq.com>
2021-01-04 22:49:14 +0800

    1111

---
+++ src/main/java/com/zhl/staz/StazApplication.java
@@ -0,0 +1,13 @@
+package com.zhl.staz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StazApplication.class, args);
+    }
+
+}
